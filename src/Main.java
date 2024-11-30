f0bc32045700381333a3547a255fdb19cf0c7e69
Kimberly Germano <kimberly252016@gmail.com>
2024-11-30 15:36:05 -0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         ocorrencias.add(new Ocorrencia("Avenida Santo Amaro", 0, 5));       // Corredor de ônibus (fora do horário)
         ocorrencias.add(new Ocorrencia("Avenida Bandeirantes", 0, 8));      // Rodízio violado (dentro do horário)
 
-        // Adicionando regras
+        // Adicionando as regras
         regras.add(new RegraVelocidade(60, "Avenida Washington Luiz"));     // Limite 60 km/h
         regras.add(new RegraCorredorOnibus(6, 23, "Avenida Santo Amaro"));  // Corredor das 6h às 23h
         regras.add(new RegraRodizio(1, new String[]{"Avenida Bandeirantes"}, 7, 10)); // Rodízio segunda-feira (7h-10h)
